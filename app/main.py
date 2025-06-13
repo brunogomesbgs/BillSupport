ccf149371632f7f75e65c845078b28b3207cee95
Bruno Silva <bug.bgs@gmail.com>
2025-06-13 16:55:30 -0300

    adjusting error messages

--- app/main.py
+++ app/main.py
@@ -40,7 +40,7 @@ async def by_legislator():
     except Exception as e:
         raise HTTPException(
             status_code=500,
-            detail=f"An error occurred while trying to classify {e}"
+            detail=f"An error occurred while trying to list by Legislator {e}"
         )
 
 @app.get('/support/by_bill')
@@ -59,5 +59,5 @@ async def by_bill():
     except Exception as e:
         raise HTTPException(
             status_code=500,
-            detail=f"An error occurred while trying to classify {e}"
+            detail=f"An error occurred while trying to list by Bill {e}"
         )
